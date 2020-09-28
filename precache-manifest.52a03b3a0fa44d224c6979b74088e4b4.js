811586e7fa2a70ba73e13553f2db5ad680af39fc
Kiriunina Natalia <kiriunina.natalia@gmail.com>
2020-09-28 21:07:40 +0300

    Updates

--- precache-manifest.52a03b3a0fa44d224c6979b74088e4b4.js
+++ precache-manifest.52a03b3a0fa44d224c6979b74088e4b4.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f5b12849c397d88ceda193509f88f6e9",
+    "revision": "97a5a30f0a65fb8787e81e6cd7333f2c",
     "url": "/goit-react-hw-02-phonebook/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-phonebook/static/css/2.3d72dcb8.chunk.css"
   },
   {
-    "revision": "f60c0074c27c3d5d5ccc",
-    "url": "/goit-react-hw-02-phonebook/static/css/main.5c1a16b2.chunk.css"
+    "revision": "fdd09c3207b97a88a6c4",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.573838c1.chunk.css"
   },
   {
     "revision": "837749b722434b3dde7c",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-phonebook/static/js/2.a219fd8b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f60c0074c27c3d5d5ccc",
-    "url": "/goit-react-hw-02-phonebook/static/js/main.7f58b4e2.chunk.js"
+    "revision": "fdd09c3207b97a88a6c4",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.1b9adc39.chunk.js"
   },
   {
     "revision": "e71ecfe6a7acd4e960cd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-phonebook/precache-manifest.431a34235028dac164e119d1419afb3d.js"
+  "/goit-react-hw-02-phonebook/precache-manifest.52a03b3a0fa44d224c6979b74088e4b4.js"
 );
 
 self.addEventListener('message', (event) => {
